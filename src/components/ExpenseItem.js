a9344ededa133a725c73a5eeb58ea057cb2b1867
Sarmad Rezayat <sarmad.re@gmail.com>
2022-02-06 00:53:28 +0100

    props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+    const expenseDate = new Date(2021, 2, 28);
+    //const expenseTitle = "Car Insurance";
+    //const expenseAmoun = 294.67;
+
     return (
         <div className='expense-item'>
-            <div >March 28th 2021</div>
+            <div >{expenseDate.toISOString()}</div>
             <div className='expense-item__description'>
-                <h2>Car Insurance</h2>
-                <div className='expense-item__price'>$294.23</div>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>${props.amount}</div>
             </div>
         </div>
     );
