95cb5adf0a2debe25c71ef365763251a05e508e8
Sarmad <sarmad.re@gmail.com>
2023-04-10 07:50:37 +0200

    section8

---
+++ section-8/src/components/Users/AddUser.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+const AddUser = (props) => {
+  const submitHandler = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <form>
+      <label htmlFor="username">Username</label>
+      <input id="username" type="text" />
+      <label htmlFor="age">Age</label>
+      <input id="age"type="number" />
+      <button type="submit">Add User</button>
+    </form>
+  );
+};
+
+export default AddUser;
