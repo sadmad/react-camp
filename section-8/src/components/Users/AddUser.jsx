810b5b529ca45fcc546cb3b4f0eadb14eab12bd5
Sarmad <sarmad.re@gmail.com>
2023-04-13 23:43:05 +0200

    add handler

--- section-8/src/components/Users/AddUser.jsx
+++ section-8/src/components/Users/AddUser.jsx
@@ -1,4 +1,8 @@
 import { useState } from "react";
+import Card from "../UI/Card";
+import classes from "./AddUser.module.css";
+import Button from "../UI/Button.jsx";
+
 
 const AddUser = (props) => {
   const submitHandler = (event) => {
@@ -6,13 +10,15 @@ const AddUser = (props) => {
   };
 
   return (
-    <form>
-      <label htmlFor="username">Username</label>
-      <input id="username" type="text" />
-      <label htmlFor="age">Age</label>
-      <input id="age"type="number" />
-      <button type="submit">Add User</button>
-    </form>
+    <Card className={classes.input}>
+      <form onSubmit={submitHandler}>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text" />
+        <label htmlFor="age">Age</label>
+        <input id="age" type="number" />
+        <Button type="submit">Add User</Button>
+      </form>
+    </Card>
   );
 };
 
